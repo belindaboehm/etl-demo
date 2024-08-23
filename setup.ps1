cedc1fd78d971efc699b1b438903163c1c24112f
belindaboehm <belinda.boehm@accenture.com>
2024-08-23 12:51:48 +0930

    update setup script

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,15 @@
 $rg = get-AzResourceGroup
 
+# deploy ADF
 New-AzResourceGroupDeployment `
   -Name adfDeployment `
   -ResourceGroupName $rg.ResourceGroupName `
-  -TemplateFile arm/parameters-sql.json `
-  -TemplateParameterFile arm/template-sql.json
+  -TemplateFile arm/template-adf.json `
+  -TemplateParameterFile arm/parameters-adf.json
+
+# deploy SQL
+New-AzResourceGroupDeployment `
+    -Name sqlDeployment `
+    -ResourceGroupName $rg.ResourceGroupName `
+    -TemplateFile arm/template-sql.json `
+    -TemplateParameterFile arm/parameters-sql.json
